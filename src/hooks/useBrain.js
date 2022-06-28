5bcea66ce0e80fc16744c0a85eb2d9573498c0a8
jorge machado <jrms_jorgesilva@hotmail.com>
2022-06-28 02:37:52 -0300

    sixth commit

---
+++ src/hooks/useBrain.js
@@ -0,0 +1,14 @@
+
+const useBrain = () => {
+
+    const brain = {
+        constitution: '90% water, 10% awesome cells',
+        power: 'unlimited',
+        usage: () => {
+            return 'whatever you want'
+        }
+    }
+  return brain
+}
+
+export default useBrain
