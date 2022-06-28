ca1c41bfbc258435cbd6f546660fb177d2bf1d28
jorge machado <jrms_jorgesilva@hotmail.com>
2022-06-28 02:18:25 -0300

    fifth commit

---
+++ src/HackerSaga.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import GameContent from './components/GameContent';
+
+function HackerSaga() {
+  return (
+    <div className="HackerSaga">
+      <GameContent />
+    </div>
+  );
+}
+
+export default HackerSaga;
